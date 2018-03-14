b3ca0b070e65f8bdba3a9779226a8f1060302b8d
L1 <aviram@sharabi.co.l>
2018-03-14 20:32:03 +0200

    Initial commit

---
+++ app/src/main/java/il/org/tikvot/aviram/tikvot/MainActivity.java
@@ -0,0 +1,13 @@
+package il.org.tikvot.aviram.tikvot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
